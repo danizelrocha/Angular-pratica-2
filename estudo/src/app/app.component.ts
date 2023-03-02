cc18d0d1a5b88bad48afc07588cc269db14089b1
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-02 13:13:41 -0300

    forms

--- estudo/src/app/app.component.ts
+++ estudo/src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-  <app-food-add></app-food-add>
-  <app-food-list></app-food-list>
+<!--   <app-food-add></app-food-add>
+  <app-food-list></app-food-list> -->
+  <app-forms></app-forms>
   <router-outlet></router-outlet>
   `,
 })
-export class AppComponent {
-  title = 'estudo';
+export class AppComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+ }
+ 
 }
--- estudo/src/app/app.module.ts
+++ estudo/src/app/app.module.ts
@@ -1,22 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+
 import { SharedModule } from "./shared/shared.module";
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    FormsComponent
   ],
-  providers: [],
-  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     SharedModule
-  ]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
